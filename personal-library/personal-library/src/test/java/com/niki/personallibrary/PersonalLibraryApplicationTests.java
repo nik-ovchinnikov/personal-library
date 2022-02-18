dd0c8693a12043a1b0e8084a39c5af3988e2922e
Nikita <nvov.nv@gamil.com>
2022-02-18 21:12:07 +0300

    first commit

---
+++ personal-library/personal-library/src/test/java/com/niki/personallibrary/PersonalLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niki.personallibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
